073b113e4d5a3379118fa565099c80d9a5f12bf2
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2016-09-30 16:26:25 +0200

    specification on long running

--- com.examples.myapp/src/main/java/com/examples/myapp/LongRunningApp.java
+++ com.examples.myapp/src/main/java/com/examples/myapp/LongRunningApp.java
@@ -4,6 +4,10 @@ package com.examples.myapp;
  * Suppose that this app takes a long time to run!
  */
 public class LongRunningApp {
+	/**
+	 * Suppose this method takes a very long time to run.
+	 * @return
+	 */
 	public String myLongRunningMethod() {
 		return "Hello World";
 	}
